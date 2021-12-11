bbdcfac2b3809b15ee1125e34f833fbf8bc172b5
Panawat.A <patjanawat@gmail.com>
2021-12-12 00:01:44 +0700

    Reaction timer game.

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <h1>Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <block v-if="isPlaying" :delay="delay" @end="endGame"/>
+  <p v-if="showResult">Reaction time: {{ score }} ms</p>
+</template>
+
+<script>
+import Block from './components/Block.vue'
+export default {
+  name: 'App',
+  components: {
+    Block
+  },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResult: false
+    }
+  },
+  methods:{
+    start() {
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+      this.showResult = false
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResult = true
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+</style>
